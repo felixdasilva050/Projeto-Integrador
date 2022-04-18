335e957b6b5050098e26566b644f3dfeabfa47d4
Guilherme <57736435+felixdasilva050@users.noreply.github.com>
2022-04-18 02:08:47 -0300

    primeiro commit

---
+++ src/test/java/com/wiprobootcamp/classeA/ProjetoFinal/ProjetoIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiprobootcamp.classeA.ProjetoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
