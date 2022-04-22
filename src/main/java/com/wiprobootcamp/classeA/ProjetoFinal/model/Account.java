38e7d3cc933546e6849023326bffb0a4852cfea9
Gustavo Santos <gustavo.santos@windowslive.com>
2022-04-21 22:26:33 -0300

    Update Account.java

--- src/main/java/com/wiprobootcamp/classeA/ProjetoFinal/model/Account.java
+++ src/main/java/com/wiprobootcamp/classeA/ProjetoFinal/model/Account.java
@@ -1,93 +1,120 @@
 package com.wiprobootcamp.classeA.ProjetoFinal.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
 import com.wiprobootcamp.classeA.ProjetoFinal.enums.AccountType;
-import com.wiprobootcamp.classeA.ProjetoFinal.enums.CustomerType;
 
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Account {
-    public String accountNumber;
-    public Double balance;
-    public CreditCard creditCard;
-    public AccountType accountType;
-    public CustomerType customerType;
-    public Customer customer;
 
+	//Definição dos atributos
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	protected Integer idAccount;
+	protected String accountNumber;
+	protected Double balance;
+	
+	@OneToOne
+	@JoinColumn(name = "id_creditcard")
+	protected CreditCard creditCard;
+	
+	protected AccountType accountType;
+	
+	//Construtor padrão
+	public Account() {
 
-    public Customer getCustomer() {return customer;}
+	}
 
-    public String getAccountNumber() {
-        return accountNumber;
-    }
+	//Construtor com os atributos
+	public Account(Integer idAccount, String accountNumber, Double balance, CreditCard creditCard,
+			AccountType accountType) {
+		super();
+		this.idAccount = idAccount;
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+		this.creditCard = creditCard;
+		this.accountType = accountType;
+	}
 
-    public Double getBalance() {
-        return balance;
-    }
+	//Getters e setters
+	public Integer getIdAccount() {
+		return idAccount;
+	}
+	
+	public void setIdAccount(Integer idAccount) {
+		this.idAccount = idAccount;
+	}
 
-    public CreditCard getCreditCard() {
-        return creditCard;
-    }
+	public String getAccountNumber() {
+		return accountNumber;
+	}
 
-    public AccountType getAccountType() {
-        return accountType;
-    }
+	public Double getBalance() {
+		return balance;
+	}
 
-    public CustomerType getCostumerType() {return customerType;}
+	public CreditCard getCreditCard() {
+		return creditCard;
+	}
 
+	public AccountType getAccountType() {
+		return accountType;
+	}
 
-    //Método utilizado para trânsferir
-    public Boolean transferMoney(Account destinationAccount, double money) {
-        if(this.getBalance() >= money){
-          this.balance = this.balance - money;
-          destinationAccount.balance = destinationAccount.balance + money;
-          return true;
-        }else{
-            System.out.println("Saldo insuficiente!");
-                return false;
-        }
-    }
-
-    //Método utilizado para consultar saldo
-    public Double checkBalance(String accountNumber) {
-        if(accountNumber.isEmpty()){
-            System.out.println("Conta não existe!");
-        } else
-            System.out.println("Seu saldo atual é de R$ " + getBalance());
-            return balance;
-    }
-
-    //Método utilizado para sacar dinheiro
-    public Boolean withdrawCash(double money) {
-        if(this.balance >= money) {
-            this.balance = getBalance() - money;
-            return true;
-        } else {
-            System.out.println("Saldo insuficiente");
-            return false;
-        }
-
-    }
-
-    //Método utilizado para depositar dinheiro
-    public void cashDeposit(double money) {
-        this.balance = getBalance() + money;
-        System.out.println("Seu saldo é de R$ " + getBalance());
-    }
-
-    //Método utilizado para consultar dados da conta
-    public void obtainAccountData(String accountNumber) {
-        System.out.println("Número da conta: " + getAccountNumber());
-        System.out.println("Saldo da conta: " + getBalance());
-        System.out.println("Cartão de crédito da conta: " + getCreditCard().getCardNumber());
-        if(this.getAccountType().equals(AccountType.CURRENT_ACCOUNT)){
-            System.out.println("Tipo da conta: CONTA CORRENTE" );
-        }else{
-            System.out.println("Tipo da conta: CONTA ESPECIAL");
-        }
-        if(this.customer.getCostumerType().equals(CustomerType.INDIVIDUAL)){
-            System.out.println("Tipo de Cliente: Pessoa Física");
-        }else{
-            System.out.println("Tipo de Cliente: Pessoa Jurídica");
-        }
-
-    }
-
+	//Equals e Hashcode
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((accountNumber == null) ? 0 : accountNumber.hashCode());
+		result = prime * result + ((accountType == null) ? 0 : accountType.hashCode());
+		result = prime * result + ((balance == null) ? 0 : balance.hashCode());
+		result = prime * result + ((creditCard == null) ? 0 : creditCard.hashCode());
+		result = prime * result + ((idAccount == null) ? 0 : idAccount.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		if (accountNumber == null) {
+			if (other.accountNumber != null)
+				return false;
+		} else if (!accountNumber.equals(other.accountNumber))
+			return false;
+		if (accountType != other.accountType)
+			return false;
+		if (balance == null) {
+			if (other.balance != null)
+				return false;
+		} else if (!balance.equals(other.balance))
+			return false;
+		if (creditCard == null) {
+			if (other.creditCard != null)
+				return false;
+		} else if (!creditCard.equals(other.creditCard))
+			return false;
+		if (idAccount == null) {
+			if (other.idAccount != null)
+				return false;
+		} else if (!idAccount.equals(other.idAccount))
+			return false;
+		return true;
+	}
 }
